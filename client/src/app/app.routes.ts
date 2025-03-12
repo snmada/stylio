758368d366bbeac84513979101266b2635c74ffe
Mădălina Sandu <sandu.madalina151@gmail.com>
2025-03-12 10:21:38 +0200

    Display product categories with selectable subcategories

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './pages/landing/landing.component';
+import { ShopComponent } from './pages/shop/shop.component';
 
 export const routes: Routes = [
     { path: '', component: LandingComponent },
+    { path: 'shop', component: ShopComponent },
 ];
