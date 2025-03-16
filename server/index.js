2923198985536361f7078b8a86779c702feb9270
Mădălina Sandu <sandu.madalina151@gmail.com>
2025-03-16 14:17:50 +0200

    [BE] Initialize Node.js project with Prisma and define schema models

---
+++ server/index.js
@@ -0,0 +1,24 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+
+const CLIENT_URL = process.env.CLIENT_URL;
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors({
+    origin: CLIENT_URL, 
+    credentials: true,
+}));
+
+app.use(express.json());
+
+app.listen(PORT, (error) => {
+    if(error) {
+        throw error;
+    } 
+    else {
+        console.log("\x1b[32m%s\x1b[0m", `Server is running on PORT: ${PORT}`);
+    }
+});
---
+++ server/prisma/migrations/20250316112457_init/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Subcategory" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "image" TEXT NOT NULL,
+    "category_id" TEXT NOT NULL,
+
+    CONSTRAINT "Subcategory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "image" TEXT NOT NULL,
+    "subcategory_id" TEXT NOT NULL,
+    "stock" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Subcategory_name_key" ON "Subcategory"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_name_key" ON "Product"("name");
+
+-- AddForeignKey
+ALTER TABLE "Subcategory" ADD CONSTRAINT "Subcategory_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "Category"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_subcategory_id_fkey" FOREIGN KEY ("subcategory_id") REFERENCES "Subcategory"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
